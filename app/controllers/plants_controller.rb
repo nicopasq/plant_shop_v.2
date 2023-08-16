c47f4c2967fa67abcbcbeea8faa28345d42292dc
Nico <nicopasquariello@gmail.com>
2023-08-16 17:53:49 -0600

    added create method to plants_controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,7 +1,26 @@
 class PlantsController < ApplicationController
+wrap_parameters format: [];
 
     def index
         plants = Plant.all
         render json: plants
+    end
+
+    def create
+        plant = Plant.create(
+            name: params[:name],
+            category: params[:category],
+            image:params[:image],
+            description:params[:description],
+            price:params[:price],
+            quantity:params[:quantity]
+        )
+        render json: plant, status: :created
+    end
+
+    private
+
+    def plant_params
+        params.permit(:name, :category, :image, :description, :price, :quantity)
     end
 end
