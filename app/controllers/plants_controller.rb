928b048c4a418d4fd0151998557a25feebcf2690
Nico <nicopasquariello@gmail.com>
2023-08-16 18:22:19 -0600

    added a show method and a plant_not_found_message private method in plants_controller.

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,10 +1,16 @@
 class PlantsController < ApplicationController
 rescue_from ActiveRecord::RecordInvalid, with: :invalid_create_message;
+rescue_from ActiveRecord::RecordNotFound, with: :plant_not_found_message;
 wrap_parameters format: [];
 
     def index
         plants = Plant.all
         render json: plants
+    end
+
+    def show
+        plant = find_plant
+        render json: plant
     end
 
     def create
@@ -13,13 +19,22 @@ wrap_parameters format: [];
     end
 
 
+
     private
 
     def plant_params
         params.permit(:name, :category, :image, :description, :price, :quantity)
     end
+
+    def find_plant
+        Plant.find(params[:id])
+    end
     
     def invalid_create_message invalid
         render json: {error: invalid.record.errors.full_messages}, status: 422
+    end
+
+    def plant_not_found_message
+        render json: {error: "Plant could not be found."}
     end
 end
