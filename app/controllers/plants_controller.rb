5819a53c1949d47d1282743bfb72ce70f872f955
Nico <nicopasquariello@gmail.com>
2023-08-16 18:17:39 -0600

    Plant validates that the name, price, image, and category are present when a new Plant is created.

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :invalid_create_message;
 wrap_parameters format: [];
 
     def index
@@ -7,13 +8,18 @@ wrap_parameters format: [];
     end
 
     def create
-        plant = Plant.create(plant_params)
+        plant = Plant.create!(plant_params)
         render json: plant, status: :created
     end
+
 
     private
 
     def plant_params
         params.permit(:name, :category, :image, :description, :price, :quantity)
+    end
+    
+    def invalid_create_message invalid
+        render json: {error: invalid.record.errors.full_messages}, status: 422
     end
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,6 @@
 class Plant < ApplicationRecord
+    validates :name, {presence: true}
+    validates :image, {presence: true}
+    validates :price, {presence: true}
+    validates :category, {presence: true}
 end
